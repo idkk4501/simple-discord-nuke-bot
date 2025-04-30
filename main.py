b5436d2eba8869f78b577b8a57fb54b1ae78274a
idkk4501 <justinirvin951@gmail.com>
2025-04-29 21:40:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import discord
+from discord.ext import commands
+import asyncio
+
+TOKEN = 'your token'   # replace ts with ur bot token
+SPAM = 'your spam message' # replace ts with ur spam message
+guild = 'your guild name' # replace with what u want the nuked server to be renamed to
+
+bot = commands.Bot(command_prefix=',' , intents=intents)
+
+@bot.event
+async def on_ready():
+  print('BOT STARTED SUCCESSFULLY')
+
